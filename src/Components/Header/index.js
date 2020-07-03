8a9dde94d108996dbadbcbeb9ea0733635783a4c
Eric Noguchi <eric.noguchi@logmein.com>
2020-07-03 03:14:53 -0400

    test header change

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,10 +1,10 @@
-import React from "react";
-import "./index.css";
+import React from 'react';
+import './index.css';
 
 function Header(props) {
   return (
     <header className="header">
-      <h1 className="header__title">Stock Market Recommender</h1>
+      <h1 className="header__title">Stock Market Recommender test</h1>
       {props.children}
     </header>
   );
